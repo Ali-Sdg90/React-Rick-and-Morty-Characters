9a1857b509c7dfd56f5e3afbf153034ede267ed1
Ali Sadeghi <ali909392@gmail.com>
2023-06-01 10:41:28 +0330

    Add base files

---
+++ login-form/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SignUp from "./components/SignUp";
+
+function App() {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    );
+}
+
+export default App;
