00797b01b98dc07fa90ae85bf1266ac9f39c1e4f
Ali Sadeghi <ali909392@gmail.com>
2023-06-05 18:27:25 +0330

    Add header api btns / Add ::selection to app

--- rick-and-morty-characters/src/components/Header.js
+++ rick-and-morty-characters/src/components/Header.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import Styles from "./Header.module.css"
+import Styles from "./Header.module.css";
 
 const Header = () => {
     return (
-        <div className={Styles.header}>
+        <div className={Styles.container}>
             <br></br>
-            <h1 className={Styles.header__text}>Rick And Morty</h1>
-            <h1 className={Styles.header__text}>Characters</h1>
+            <h1 className={Styles.container__text}>Rick And Morty</h1>
+            <h1 className={Styles.container__text}>Characters</h1>
+            <div>
+                <a
+                    href="https://rickandmortyapi.com/api/character"
+                    target="_new"
+                >
+                    826 Characters
+                </a>
+                <a href="https://rickandmortyapi.com/" target="_new">
+                    Rick & Morty api
+                </a>
+                <a
+                    href="https://rickandmortyapi.com/api/location"
+                    target="_new"
+                >
+                    126 Locations
+                </a>
+            </div>
         </div>
     );
 };
